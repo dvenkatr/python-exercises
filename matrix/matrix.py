4979450d4ae567c3e3d95a537b84491a614094d1
Deepa <deepa@accosted.net>
2021-04-17 19:55:35 -0400

    Sequence exercises: make matrix solution more readable

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -2,8 +2,10 @@ class Matrix:
     def __init__(self, matrix_string):
 
         self.matrix = []
-        for row in matrix_string.split('\n'):
-            self.matrix.append([int(col) for col in row.split()])
+        for row_string in matrix_string.split('\n'):
+            row = list(map(int, row_string.split()))
+            self.matrix.append(row)
+            # self.matrix.append([int(col) for col in row.split()])
             # self.matrix.append(list(map(int, row.split())))
 
     def row(self, index):
@@ -11,3 +13,5 @@ class Matrix:
 
     def column(self, index):
         return [row[index - 1] for row in self.matrix]
+
+x = Matrix("89 1903 3\n18 3 1\n9 4 800")
